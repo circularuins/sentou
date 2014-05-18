b47731d274287690debb009f80d3f2f3911eb683
natsuhiko.wake <natsuhiko.wake@GX1487.local>
2014-05-18 11:44:55 +0900

    折れ線グラフに変更。複数戦士にも対応

--- app/controllers/sentouryoku_controller.rb
+++ app/controllers/sentouryoku_controller.rb
@@ -13,15 +13,43 @@ class SentouryokuController < ApplicationController
       '756' => 380,
     }
 
-    gokuu_kai = [
-                 { age: 737, power: 2 },
-                 { age: 738, power: 3 },
-                 { age: 749, power: 12 },
-                 { age: 750, power: 150 },
-                 { age: 753, power: 270 },
-                 { age: 756, power: 380 },
-                ]
-      
-    render :json => gokuu_kai
+    zsensi = {
+      gokuu: [
+              { age: '737/7/7', power: 2 },
+              { age: '738/7/7', power: 3 },
+              { age: '749/9/1', power: 100 },
+              { age: '749/9/10', power: 12 },
+              { age: '750/5/7', power: 1000 },
+              { age: '750/5/8', power: 220 },
+              { age: '750/5/9', power: 230 },
+              { age: '750/5/12', power: 300 },
+              { age: '753/5/7', power: 360 },
+              { age: '753/5/8', power: 380 },
+              { age: '753/5/9', power: 780 },
+              { age: '756/5/7', power: 910 },
+              { age: '761/10/12', power: 1040 },
+              { age: '762/4/29', power: 2275 },
+              { age: '762/11/3', power: 32000 },
+              { age: '762/12/18', power: 10000 },
+              { age: '762/12/24', power: 150000000 },
+              { age: '764/8/7', power: 200000000 },
+              { age: '767/5/12', power: 3375000000 },
+              { age: '767/5/26', power: 60000000000 },
+              { age: '767/6/30', power: 92250000000 },
+              { age: '774/5/8', power: 1200000000000 },
+              { age: '784/5/7', power: 2000000000000 }
+             ],
+      pikkoro: [
+                { age: '753/5/9', power: 260 },
+                { age: '756/5/7', power: 365 },
+                { age: '784/5/7', power: 27000000000 }
+               ],
+      vegeta: [
+               { age: '762/11/3', power: 180000 },
+               { age: '784/5/7', power: 300000000000 }
+              ],
+    }
+
+    render :json => zsensi
   end
 end
