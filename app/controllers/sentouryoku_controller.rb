28e47db9566bac42cb91b26213966c21c6b998f6
natsuhiko.wake <natsuhiko.wake@GX1487.local>
2014-05-26 13:07:49 +0900

    delete some

--- app/controllers/sentouryoku_controller.rb
+++ app/controllers/sentouryoku_controller.rb
@@ -4,15 +4,6 @@ class SentouryokuController < ApplicationController
   end
 
   def sensi
-    gokuu = {
-      '737' => 2,
-      '738' => 3,
-      '749' => 12,
-      '750' => 150,
-      '753' => 270,
-      '756' => 380,
-    }
-
     zsensi = {
       gokuu: [
               { age: '737/7/7', power: 2 },
